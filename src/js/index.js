0f0f51ac30ed9c0ebb62c4a947b608678dbbbded
nickparsley <nickparsley5@gmail.com>
2020-05-31 05:43:00 +0000

    ES6 module test

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+import str from './models/Search';
+// import { add as a, multiply as m, ID } from './views/searchView';
+import * as searchView from './views/searchView';
+console.log(`Using imprted functions! ${searchView.add(searchview.ID, 2)} and ${searchView.multiplym(3, 5)}. ${str}`);
