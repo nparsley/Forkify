a982cd7bf4a69a0b6ee7cbb84eb504ee0ec6e7e0
nickparsley <nickparsley5@gmail.com>
2020-06-05 01:52:51 +0000

    Building the recipe view

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
 import { elements, renderLoader, clearLoader } from './views/base';
+import * as recipeView from './views/recipeView';
 
 
 /**Global State of the app 
@@ -70,7 +71,8 @@ const controlRecipe = async () => {
 
     if (id) {
         //Prepare UI for changes
-
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
         //Create new recipe object
         state.recipe = new Recipe(id);
 
@@ -85,7 +87,8 @@ const controlRecipe = async () => {
             state.recipe.calcServings();
 
             //Render recipe
-            console.log(state.recipe);
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
         } catch (err) {
             alert('Error processing recipe!');
         }
