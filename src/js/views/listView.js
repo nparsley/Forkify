022ef6d90062fa143cafff798e84c8aa6d720143
nickparsley <nickparsley5@gmail.com>
2020-06-07 08:28:11 +0000

    Building the shopping list controller

--- src/js/views/listView.js
+++ src/js/views/listView.js
@@ -1,7 +1,7 @@
 import { elements } from './base';
 
 export const renderItem = item => {
-    const markup = '
+    const markup = `
         <li class="shopping__item" data-itemid=${item.id}>
             <div class="shopping__count">
                 <input type="number" value="${item.count}" step="${item.count}" class="shopping__count-value">
@@ -14,7 +14,8 @@ export const renderItem = item => {
                 </svg>
             </button>
         </li>
-    ';
+    `;
+    elements.shopping.insertAdjacentHTML('beforeend', markup);
 };
 
 export const deleteItem = id => {
