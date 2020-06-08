e8acc864b732260fc919bc78826805b66d4d08e2
nickparsley <nickparsley5@gmail.com>
2020-06-08 05:12:38 +0000

    Building the likes view

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -22,3 +22,10 @@ export default class Likes {
         return this.likes.length;
     }
 }
+
+
+
+
+
+
+
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -17,7 +17,7 @@ export const highlightSelected = id => {
         el.classList.remove('results__link--active');
     });
 
-    document.querySelector(`a[href="#${id}"]`).classList.add('results__link--active');
+    document.querySelector(`.results__link[href="#${id}"]`).classList.add('results__link--active');
 }
 
 /*
@@ -26,7 +26,7 @@ acc: 0 / acc + curr.length = 5 / newTitle = ['Pasta']
 acc: 5 / acc + curr.length = 9 / newTitle = ['Pasta', 'with']
 acc: 9 / acc + curr.length = 15 / newTitle = ['Pasta', 'with', 'tomato']
 */
-const limitRecipeTitle = (title, limit = 17) => {
+export const limitRecipeTitle = (title, limit = 17) => {
     const newTitle = [];
     if (title.length > limit) {
         title.split(' ').reduce((acc, cur) => {
